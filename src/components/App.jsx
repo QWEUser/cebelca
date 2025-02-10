138256c176a5682bcc03f045cd38438ce7ac382f
QWEUser <lovro.gorse@gmail.com>
2025-02-10 21:26:50 +0100

    fix: daily game restart bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ import EndOfGame from "./EndOfGame";
 import Intro from "./Intro";
 
 // app version
-const appVersion = "1.1.5.p60";
+const appVersion = "1.1.6.p100";
 
 // yearDay is a string made from current year and current day in year, for example "2024" (year) + "141" (current day in year) = "2024141"
 const createTodayYearDay = () => {
@@ -64,7 +64,7 @@ const congratulationsWords = [
 function minPointsGame(pangramNumber, gameType) {
   // console.log("pangramNumber: " + pangramNumber);
   // define minimum points in the game
-  const minimumPoints = 60;
+  const minimumPoints = 100;
   // create an initial Pangram either from a daily game or at random
   let initialPangram = "čebelica";
   if (gameType === "daily") {
@@ -259,7 +259,7 @@ function reducer(state, action) {
       return {
         ...initialState,
         darkMode: state.darkMode,
-        yearDay: todayYearDay,
+        yearDay: todayYearDay || state.yearDay,
         solutionsArray: gameParams["solutionsArray"],
         gameCenterLetter: gameParams["gameCenterLetter"],
         totalScore: gameParams["totalScore"],
